10b5ea42ba38cafde685f9e662aa5a1f1e95f235
xuanyel <102975182+xuanyel@users.noreply.github.com>
2022-04-05 10:48:49 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,8 +2,8 @@
 $type = $_REQUEST['type'];  //类型：1=测试 空=正常生产环境
 // 连接libvirt（kvm）服务器
 if ($type == 1 || $type == "1") {  //测试环境
-    $url = "qemu+tcp://your_server_ip/system"; // Libvirt-kvm服务器地址(方法：tcp)
-    $aaasasl = array(VIR_CRED_AUTHNAME => "youruser", VIR_CRED_PASSPHRASE => "userpassword");  //通过sasl认证TCP连接，VIR_CRED_AUTHNAME为sasl2账号，VIR_CRED_PASSPHRASE为sasl2密码（禁止外泄）
+    $url = "qemu+tcp://192.168.1.104/system"; // Libvirt-kvm服务器地址(方法：tcp)
+    $aaasasl = array(VIR_CRED_AUTHNAME => "XXXH", VIR_CRED_PASSPHRASE => "XXXH@KvmServer");  //通过sasl认证TCP连接，VIR_CRED_AUTHNAME为sasl2账号，VIR_CRED_PASSPHRASE为sasl2密码（禁止外泄）
     $conn = libvirt_connect($url, false, $aaasasl); //连接kvm服务器（TCP+SASL2）
     if ($conn == false) {
         echo "{\"code\":\"400\",\"msg\":\"" . libvirt_get_last_error() . "\"}"; //json返回值（失败）
@@ -12,14 +12,14 @@ if ($type == 1 || $type == "1") {  //测试环境
     }
 }
 else {  //正常环境
-    $url = "qemu+tcp://your_server_ip/system"; // Libvirt-kvm服务器地址(方法：tcp)
-    $aaasasl = array(VIR_CRED_AUTHNAME => "youruser", VIR_CRED_PASSPHRASE => "userpassword");  //通过sasl认证TCP连接，VIR_CRED_AUTHNAME为sasl2账号，VIR_CRED_PASSPHRASE为sasl2密码（禁止外泄）
+    $url = "qemu+tcp://192.168.1.104/system"; // Libvirt-kvm服务器地址(方法：tcp)
+    $aaasasl = array(VIR_CRED_AUTHNAME => "XXXH", VIR_CRED_PASSPHRASE => "XXXH@KvmServer");  //通过sasl认证TCP连接，VIR_CRED_AUTHNAME为sasl2账号，VIR_CRED_PASSPHRASE为sasl2密码（禁止外泄）
     $conn = libvirt_connect($url, false, $aaasasl); //连接kvm服务器（TCP+SASL2）
     if ($conn == false) {
+        $connect_state = "fail";
         echo "{\"code\":\"400\",\"msg\":\"" . libvirt_get_last_error() . "\"}"; //json返回值（失败）
     } else {   // 否则 （成功）
         //echo "{\"code\":\"200\",\"msg\":\"success\"}";  //json返回值（成功）
-        $connect = "success";
+        $connect_state = "success";
     }
 }
-?>
